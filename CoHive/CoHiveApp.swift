4e93bb0f90091a317efcb131c12a3d9b7729a4fc
apolyeti <azhandarveen@gmail.com>
2024-03-23 00:32:27 -0700

    Initial Commit

---
+++ CoHive/CoHiveApp.swift
@@ -0,0 +1,32 @@
+//
+//  CoHiveApp.swift
+//  CoHive
+//
+//  Created by Arveen Azhand on 3/23/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CoHiveApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CoHive/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CoHive
+//
+//  Created by Arveen Azhand on 3/23/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
