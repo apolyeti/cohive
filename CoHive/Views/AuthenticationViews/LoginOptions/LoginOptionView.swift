172a70f9af4c98b5c8d6fa2a9c45f9ee0ad7bfc3
apolyeti <azhandarveen@gmail.com>
2024-03-28 00:21:28 -0700

    Making reusable UI components and enhancing file structure, as well as adding custom font

---
+++ CoHive/Views/AuthenticationViews/LoginOptions/LoginOptionView.swift
@@ -0,0 +1,35 @@
+//
+//  LoginOptionView.swift
+//  CoHive
+//
+//  Created by Arveen Azhand on 3/28/24.
+//
+
+import SwiftUI
+
+struct LoginOptionView: View {
+    
+    let label: String
+    let function: () -> Void
+    
+    var body: some View {
+        Button(action: function, label: {
+            ZStack {
+                Rectangle()
+                    .frame(width: 270, height: 35)
+                    .foregroundColor(Color("ButtonColor"))
+                    .border(Color.accentColor)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: 3, y: 3)
+                Text(label)
+            }
+        })
+        .padding(5)
+    }
+}
+
+#Preview {
+    LoginOptionView(label: "Test!") {
+        print("test!")
+    }
+}
