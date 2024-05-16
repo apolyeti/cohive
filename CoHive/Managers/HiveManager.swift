eef2467d8a1309591e29c001dbbb28fb679e0fdc
apolyeti <azhandarveen@gmail.com>
2024-05-15 22:28:53 -0700

    create a hive manager and begin hive creation process

---
+++ CoHive/Managers/HiveManager.swift
@@ -0,0 +1,8 @@
+//
+//  HiveManager.swift
+//  CoHive
+//
+//  Created by Arveen Azhand on 5/15/24.
+//
+
+import Foundation
---
+++ CoHive/Views/SessionViews/CreateHiveView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateHiveView.swift
+//  CoHive
+//
+//  Created by Arveen Azhand on 5/15/24.
+//
+
+import SwiftUI
+
+struct CreateHiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateHiveView()
+}
