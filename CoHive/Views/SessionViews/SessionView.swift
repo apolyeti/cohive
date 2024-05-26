5dbf82beaf8dcc19aa82c0d1ad264345117739b4
apolyeti <azhandarveen@gmail.com>
2024-05-25 19:10:32 -0700

    feat: create a session view for users when logging in, so they either are viewing their hive or prompted to create or join one

---
+++ CoHive/Views/SessionViews/SessionView.swift
@@ -0,0 +1,18 @@
+//
+//  SessionView.swift
+//  CoHive
+//
+//  Created by Arveen Azhand on 5/25/24.
+//
+
+import SwiftUI
+
+struct SessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SessionView()
+}
